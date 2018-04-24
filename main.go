d864d61a60a88a1069f454a707c887bc9b80c34e
empijei <robclap8@gmail.com>
2018-04-23 18:22:36 -0700

    Now warns if base si out of range

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
 	if *value == "" && flag.Arg(0) != "" {
 		*value = flag.Arg(0)
 	}
+	if *fromBase < 2 || *fromBase > 36 || *toBase < 2 || *toBase > 36 {
+		log.Fatal("base not supported, base should be between 2 and 36 included")
+	}
 	v, err := strconv.ParseInt(*value, *fromBase, 64)
 	if err != nil {
 		log.Fatal("Value is invalid or not specified")
